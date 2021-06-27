0282289362e970da6fd3f75403ea08ee5d7ee35e
hamed <hamediran1999@gmail.com>
2021-06-27 17:59:03 +0430

    add 404 page

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "hello it's me"
+def main_page():
+    return jsonify(msg="hello it's me"), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(msg="sorry this resource was not found"),404
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
