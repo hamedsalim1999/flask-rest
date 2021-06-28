fad95afbbcfeee57c52ece860033f553612f8a30
hamed <hamediran1999@gmail.com>
2021-06-28 15:02:42 +0430

    add static route varibale rest api

--- app.py
+++ app.py
@@ -12,13 +12,23 @@ def not_found():
 
 
 @app.route('/paramerts')
-def paramets():
+def paramets_dy ():
     name=request.args.get('name')
     age=int(request.args.get('age'))
     if age < 18:
         return jsonify(msg=f"sorry {name} you are not old enough") , 401
     else:
         return jsonify(msg=f"welcome {name}"), 200
+
+
+@app.route('/url/<string:name>/<int:age>')
+def paramets_st(name: str,age: int):
+
+    if age < 18:
+        return jsonify(msg=f"sorry {name} you are not old enough") , 401
+    else:
+        return jsonify(msg=f"welcome {name}"), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
