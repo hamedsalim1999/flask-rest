e394d757ec0b263785aba46fb978eaccf50ab1cb
hamed <hamediran1999@gmail.com>
2021-07-04 12:20:58 +0430

    add hand filtering aitems

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-from flask import Flask
+from flask import Flask ,request
 from flask_restful import Api , Resource
 
 app = Flask(__name__)
 api = Api(app)
 datas=[]
+
+class ItemList(Resource):
+    def get(self):
+        pass
+
 class Student(Resource):
 
     def get(self,name):
-        for i in datas:
-            if i['name'] == name:
-                return name , 200
-            
+        item = next(filter(lambda x : x['name'] == name , datas),None) 
+        if item:
+            return item , 200 if item is not None else 404
+        
         return {'items' : None} , 404
         # return{"Student":name}
     def post(self,name):
-        data = {}
-        datas.append({'name':name})
+        data = request.get_json()
+        datas.append({'name':name,'price':data['price']})
         return datas , 201
 
-        
+api.add_resource(ItemList,'/items')
 api.add_resource(Student , '/item/<string:name>')
 
 if __name__ == '__main__':
