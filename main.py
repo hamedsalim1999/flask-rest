aaf081722c4ec1d7871ddfcd1fce3e3051c3c8b7
hamed <hamediran1999@gmail.com>
2021-07-04 11:25:11 +0430

    write firs class base api view

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from website import create_app
+from flask import Flask
+from flask_restful import Api , Resource
 
-app = create_app()
+app = Flask(__name__)
+api = Api(app)
+
+class Student(Resource):
+    def get(self,name):
+        return{"Student":name}
+
+api.add_resource(Student , '/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
