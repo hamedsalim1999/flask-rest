901ff0593cf856855d7008878f32c9b2cdf2e561
hamed <hamediran1999@gmail.com>
2021-07-07 16:30:42 +0430

    add pars request for update data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask ,request
-from flask_restful import Api , Resource
+from flask_restful import Api , Resource , reqparse
 from flask_jwt import JWT , jwt_required
 from secrety import authenticate,identity
 
@@ -36,8 +36,14 @@ class Student(Resource):
         else:
             return "we dont have this item "
     def put(self,name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+        type=float,
+        required=True,
+        help="this field not can be blank"
+        )
         item = next(filter(lambda x : x['name'] == name , datas),None) 
-        data = request.get_json()
+        data = parser.parse_args()
         if item:
             item.update({'name':name,'price':data['price']})
             return f"items was update"
