59f576950711df91cf1baaee09342cabf57e7482
hamed <hamediran1999@gmail.com>
2021-07-10 15:55:26 +0430

    remove none return from user find by usern mae

--- secrety.py
+++ secrety.py
@@ -1,15 +1,9 @@
 from users import User
 
 
-user = [
-    User(1,'bob','rust')
-]
-username_mapping={u.username: u for u in user}
-userid_mapping = {u.id: u for u in user}
-
 
 def authenticate(username , password):
-    user = User.find_by_username(username,None)
+    user = User.find_by_username(username)
     if user and user.password == password:
         return user
 
