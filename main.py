de6bf6f32c2c2e53d1ef94476b4af811f98f3b4b
hamed <hamediran1999@gmail.com>
2021-07-11 16:54:55 +0430

    add get all items list url

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_jwt import JWT 
 from secrety import authenticate,identity
 from users import UserRegister
-from items import Item
+from items import Item,ItemList
 
 
 app = Flask(__name__)
@@ -15,6 +15,6 @@ jwt = JWT(app,authenticate,identity)
 
 api.add_resource(Item , '/item/<string:name>')
 api.add_resource(UserRegister,'/singup')
-
+api.add_resource(ItemList,'/')
 if __name__ == '__main__':
     app.run(debug=True)
