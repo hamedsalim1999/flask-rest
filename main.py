1798797882740be3d6b6213b5e8493ccc8c202dc
hamed <hamediran1999@gmail.com>
2021-07-12 17:09:10 +0430

    change stucher directory

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from flask_restful import Api
 from flask_jwt import JWT 
 from secrety import authenticate,identity
 from users import UserRegister
-from items import Item,ItemList
+from views.items import Item
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
@@ -11,10 +13,15 @@ api = Api(app)
 app.config['SECRET_KEY'] ='thisissecret'
 jwt = JWT(app,authenticate,identity)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
+
 
 
 api.add_resource(Item , '/item/<string:name>')
-api.add_resource(UserRegister,'/singup')
-api.add_resource(ItemList,'/')
+# api.add_resource(UserRegister,'/singup')
+# api.add_resource(ItemList,'/')
 if __name__ == '__main__':
     app.run(debug=True)
