849fdb23ad5bbe0d76d8e83e1b71c4149502cc57
hamed <hamediran1999@gmail.com>
2021-07-14 12:43:13 +0430

    cahng user vies path

--- secrety.py
+++ secrety.py
@@ -1,12 +1,9 @@
-from users import User
-
-
+from views.users import User
 
 def authenticate(username , password):
     user = User.find_by_username(username)
     if user and user.password == password:
         return user
-
         
 def identity(payload):
     user_id = payload['identity']
