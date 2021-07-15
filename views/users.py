f619753f1c80c3c4aa4cee00cd95bda17808ab0e
hamed <hamediran1999@gmail.com>
2021-07-15 12:19:20 +0430

    import user modesl on views

--- views/users.py
+++ views/users.py
@@ -1,37 +1,6 @@
 import sqlite3
+from .models.users import UserModel
 from flask_restful import Resource,reqparse
-class User:
-    def __init__(self,_id,username,password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def find_by_username(cls,username):
-        connect = sqlite3.connect('mydb.db')
-        cursor = connect.cursor()
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query,(username,))
-        row = result.fetchone()
-        if row:
-            user=cls(*row)
-        else:
-            user= None
-        connect.close()
-        return user
-    @classmethod
-    def find_by_id(cls,_id):
-        connect = sqlite3.connect('mydb.db')
-        cursor = connect.cursor()
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query,(_id,))
-        row = result.fetchone()
-        if row:
-            user=cls(*row)
-        else:
-            user= None
-        connect.close()
-        return user
 
 
 class UserRegister(Resource):
@@ -49,7 +18,7 @@ class UserRegister(Resource):
         )
     def post(self):
         data = self.parser.parse_args()
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {"MSG":"We have this user naem"}
         else:
             connect = sqlite3.connect('mydb.db')
