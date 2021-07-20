c8f18d2d0a51b4699f5761e0f7825ba35e41bcd0
hamed <hamediran1999@gmail.com>
2021-07-20 17:51:14 +0430

    add function to return all list

--- models/items.py
+++ models/items.py
@@ -14,7 +14,10 @@ class ItemModel(db.Model):
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+    @classmethod
+    def get_all_row(cls):
 
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
