f20ddf685e9e89498d38b32c71bfe836b1bc5aa9
hamed <hamediran1999@gmail.com>
2021-07-21 16:49:20 +0430

    change get all row data method from list data to single line loop

--- models/store.py
+++ models/store.py
@@ -6,19 +6,21 @@ class StoreModel(db.Model):
     name = db.Column(db.String(128), unique=True, nullable=False)
     # foreignkey
     items = db.relationship("ItemModel", lazy='dynamic')
+    
     def __init__(self, name):
         self.name = name
 
-
     def json(self):
         return {'name': self.name, "items":[item.json() for item in self.items.all()]}
+        
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+
     @classmethod
     def get_all_row(cls):
+        return {'stores': [x.json() for x in StoreModel.query.all()]}
 
-        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
