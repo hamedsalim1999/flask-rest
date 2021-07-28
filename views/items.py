d939adf032db3b36da4e2f4f3850cfc3e8593e44
hamed <hamediran1999@gmail.com>
2021-07-28 11:09:09 +0430

    add pretmison for delete user

--- views/items.py
+++ views/items.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource,reqparse
-from flask_jwt_extended import jwt_required,create_access_token
+from flask_jwt_extended import jwt_required,create_access_token,get_jwt
 
 from models.items import ItemModel
 class Item(Resource):
@@ -16,7 +16,7 @@ class Item(Resource):
         help="every item need store id "
         )
     
-    @jwt_required
+    @jwt_required()
     def get(self,name):
         item =  ItemModel.find_by_name(name)
         if item : 
@@ -36,12 +36,11 @@ class Item(Resource):
         except:
             return {"msg":"An error occurred inserting the item"}
        
-    @jwt_required
+    @jwt_required()
     def delete (self,name):
-        additional_claims = {"id": 1}
-        access_token = create_access_token(name, additional_claims=additional_claims)
+        claims = get_jwt()
         item = ItemModel.find_by_name(name)
-        if not access_token['is_admin']:
+        if not claims['is_admin']:
             return{"msg": "your don't have permission "},401
         if item:
             item.delete_from_db()
