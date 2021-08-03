4f839f238dfe516c70b071e1b8e7ba757868c5df
hamed <hamediran1999@gmail.com>
2021-08-03 16:54:39 +0430

    hint data type for json resualt

--- models/store.py
+++ models/store.py
@@ -1,5 +1,7 @@
 from db import db
-from typing import List , Dict
+from .items import ItemJSON
+from typing import List , Dict, Union
+StoreJSON = Dict[str,Union[str,List[ItemJSON]]]
 class StoreModel(db.Model):
     __tablename__="store"
     id = db.Column(db.Integer, primary_key=True)
@@ -10,11 +12,11 @@ class StoreModel(db.Model):
     def __init__(self, name:str):
         self.name = name
 
-    def json(self)-> Dict:
+    def json(self)-> StoreJSON:
         return {'name': self.name, "items":[item.json() for item in self.items.all()]}
         
     @classmethod
-    def find_by_name(cls, name:str):
+    def find_by_name(cls, name:str)-> "StoreModel":
         return cls.query.filter_by(name=name)
 
     @classmethod
