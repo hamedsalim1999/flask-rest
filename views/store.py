16822debad2cfb06ae7ff0c1e363f9ba453243c8
hamed <hamediran1999@gmail.com>
2021-08-03 16:17:47 +0430

    return http code error

--- views/store.py
+++ views/store.py
@@ -9,21 +9,21 @@ class Store(Resource):
     def get(self,name):
         store =  StoreModel.find_by_name(name)
         if store : 
-            return store.json()
+            return store.json(),200
         return {"msg":"store not found"} , 404
         
 
     def post (self,name):
         store = StoreModel.find_by_name(name)
         if store :
-            return {"msg":f"{store.json()} already exists"}
+            return {"msg":f"{store.json()} already exists"},200
 
         store = StoreModel(name)
         try:
             store.save_to_db()
-            return {"msg":f"item was crate {store.json()}"}
+            return {"msg":f"item was crate {store.json()}"},201
         except:
-            return {"msg":"An error occurred inserting the item"}
+            return {"msg":"An error occurred inserting the item"},404
        
 
     def delete (self,name):
@@ -31,9 +31,9 @@ class Store(Resource):
         
         if store:
             store.delete_from_db()
-            return {"msg":"store was deleted"}
-        return {"msg":"store wasn't exists"}
+            return {"msg":"store was deleted"},202
+        return {"msg":"store wasn't exists"},404 
         
 class StoreList(Resource):
     def get(self):
-        return StoreModel.get_all_row()
+        return StoreModel.get_all_row(),200
