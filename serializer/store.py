795d9d561431f0f3c30d990f4189432ad2cba99a
hamed <hamediran1999@gmail.com>
2021-08-09 15:23:33 +0430

    load only store and add dump only id

--- serializer/store.py
+++ serializer/store.py
@@ -1,8 +1,10 @@
-# from models.store import StoreModel
-# from ma import ma
-# class StoreSchema(ma.SQLAlchemyAutoSchema):
-#     class Meta:
-#         model=StoreModel
-#         include_relationships = True
+from models.store import StoreModel
+from ma import ma
+class StoreSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        dump_only = ('id',)
+        load_only=('store',)
+        model=StoreModel
+        include_relationships = True
 
     
