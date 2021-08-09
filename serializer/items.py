67b1f4c4aa5f5a5f1c0ce89187d27ccf60b772c9
hamed <hamediran1999@gmail.com>
2021-08-09 15:23:17 +0430

    dump onli id

--- serializer/items.py
+++ serializer/items.py
@@ -3,6 +3,7 @@ from models.store import StoreModel
 from ma import ma
 class ItemSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
+        dump_only = ('id',)
         model = ItemModel
         include_relationships = True
   
