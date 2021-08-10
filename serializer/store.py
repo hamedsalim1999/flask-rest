3ffa054857047d1114e5411e07174eb5fe7b0622
hamed <hamediran1999@gmail.com>
2021-08-10 16:37:17 +0430

    add Nestedmethod

--- serializer/store.py
+++ serializer/store.py
@@ -1,6 +1,9 @@
 from models.store import StoreModel
+from models.items import ItemModel
+from serializer.items import ItemSchema
 from ma import ma
 class StoreSchema(ma.SQLAlchemyAutoSchema):
+    item = ma.Nested(ItemSchema,many=True)
     class Meta:
         dump_only = ('id',)
         load_only=('store',)
