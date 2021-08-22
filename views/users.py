4d4292fa5490b104739b9bac3b1c5d24d5a9ad24
hamed <hamediran1999@gmail.com>
2021-08-22 17:02:48 +0430

    remove extra libery

--- views/users.py
+++ views/users.py
@@ -1,7 +1,6 @@
 from models.users import UserModel
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token,create_refresh_token
-from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import create_access_token,create_refresh_token,jwt_required, get_jwt
 from serializer.users import UserSchema 
 from flask import request
 import traceback
