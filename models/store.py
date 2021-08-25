11c70e4d9239000414e31d06d110195c3dd85b59
hamed <hamediran1999@gmail.com>
2021-08-25 18:49:26 +0430

    store mode linitil

--- models/store.py
+++ models/store.py
@@ -7,15 +7,17 @@ class StoreModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
     # foreignkey
-    items = db.relationship("ItemModel", lazy='dynamic')
+    items = db.relationship("ItemModel", lazy='joined')
+    def __init__(self, name):
+        self.name = name
+      
+    @classmethod
+    def find_by_name(self, name:str)-> "StoreModel":
+        return self.query.filter_by(name=name).first()
     
     @classmethod
-    def find_by_name(cls, name:str)-> "StoreModel":
-        return cls.query.filter_by(name=name).first()
-    
-    @classmethod
-    def get_all_row(cls) -> List:
-        return cls.query.all()
+    def get_all_row(self) -> List:
+        return self.query.all()
 
     def save_to_db(self)-> None:
         db.session.add(self)
