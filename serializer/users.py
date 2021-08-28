62a33dfc588b95b799babf276f00249df86b3b6c
hamed <hamediran1999@gmail.com>
2021-08-28 10:43:30 +0430

    change dump only and load only

--- serializer/users.py
+++ serializer/users.py
@@ -1,10 +1,10 @@
 from models.users import UserModel
 from ma import ma
 class UserSchema(ma.SQLAlchemyAutoSchema):
+
     class Meta:
-        id = ma.auto_field(dump_only=True)
-        activate = ma.auto_field(dump_only=True)
         load_only=('password',)
+        dump_only=('id','activate',)
         load_instance = True
         include_relationships = True
         model = UserModel
