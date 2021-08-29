90c89303c310bf880a4861bfbf947d9354d55b36
hamed <hamediran1999@gmail.com>
2021-08-29 10:51:56 +0430

    fix send email issue on mailgun lib

--- lib/mailgun.py
+++ lib/mailgun.py
@@ -4,19 +4,19 @@ from typing import List
 from decouple import config
 from flask import request , url_for
 class MailGun:
-    def __init__(self):
-        self.ADDRES=config('ADDRES')
-        self.FROM_TITLE=config('FROM_TITLE')
-        self.FROM_EMAIL =config('FROM_EMAIL')
-        self.PRIVATE_API=config('PRIVATE_API_KEY')    
-        
-    def send_email(self,email:List[str],subject:str,text:str,html:str) -> Response:
+ 
+    ADDRES=config('ADDRES')
+    FROM_TITLE=config('FROM_TITLE')
+    FROM_EMAIL =config('FROM_EMAIL')
+    PRIVATE_API=config('PRIVATE_API_KEY')    
+    @classmethod 
+    def send_email(cls,email:List[str],subject:str,text:str,html:str) -> Response:
         
         return post(
-        f"https://api.mailgun.net/v3/{self.ADDRES}/messages",
-        auth=("api", self.PRIVATE_API),
+        f"https://api.mailgun.net/v3/{cls.ADDRES}/messages",
+        auth=("api", cls.PRIVATE_API),
         data={
-            "from": f"{self.FROM_TITLE}<{self.FROM_EMAIL}>",
+            "from": f"{cls.FROM_TITLE}<{cls.FROM_EMAIL}>",
             "to":email,
             "subject": subject,
             "text":text,
