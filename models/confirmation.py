157d6d291fc6cd1c57370f2367ccd35c49545cc3
hamed <hamediran1999@gmail.com>
2021-09-01 15:44:09 +0430

    write constructors

--- models/confirmation.py
+++ models/confirmation.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-from datetime import datetime
+from datetime import datetime,timedelta
 from db import db
 
 class ConfirmationModel(db.Model):
@@ -9,3 +9,10 @@ class ConfirmationModel(db.Model):
     confirmed = db.Column(db.Boolean,nullable=False)
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"),nullable=False)
     user = db.relationship("UserModel")
+    def __init__(self,user_id : int,**kwargs) -> None:
+        super().__init__(**kwargs)
+        self.user_id = user_id
+        self.id = uuid4().hex
+        self.expire_at=int(datetime.now()+timedelta(minutes=30))
+        self.confirmed=False
+        
