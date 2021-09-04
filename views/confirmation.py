f4d9fa9366576cf1ac6080a5c10928e7a93d160a
hamed <hamediran1999@gmail.com>
2021-09-04 11:16:48 +0430

    start confirmation view

---
+++ views/confirmation.py
@@ -0,0 +1,19 @@
+from flask_restful import Resource
+
+
+
+class Confirmation(Resource):
+    @classmethod
+    def get(cls):
+        pass
+    
+
+class ConfirmationByUser(Resource):
+    @classmethod
+    def get(cls):
+        pass
+    
+    
+    @classmethod
+    def post(cls):
+        pass
