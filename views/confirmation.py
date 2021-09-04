1e75c595e661900168c1c71e69bce7b37e128eae
hamed <hamediran1999@gmail.com>
2021-09-04 11:26:31 +0430

    wrtie Confirmation view for save Confirmation on db

--- views/confirmation.py
+++ views/confirmation.py
@@ -1,12 +1,25 @@
 from flask_restful import Resource
-
+from models.confirmation import ConfirmationModel
 
 
 class Confirmation(Resource):
     @classmethod
-    def get(cls):
-        pass
-    
+    def get(cls,confirmation_id : str):
+        confirmation = ConfirmationModel.find_by_id(confirmation_id)
+        if not confirmation:
+            return{"msg":"not found"},404
+        
+        if confirmation.expired:
+            return {"msg":"this message was expired"},400
+        
+        if confirmation.confirmed:
+            return{"msg":"was confirmed"},400
+        
+        confirmation.confirmed = True
+        confirmation.save_to_db()
+        return {
+            "msg":f"{confirmation.user.email}",
+            },200
 
 class ConfirmationByUser(Resource):
     @classmethod
